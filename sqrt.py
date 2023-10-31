3030e356a440b1fcbfd4dc6aa9a9b9d46ced8087
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-10-31 19:54:07 +0200

    py

---
+++ sqrt.py
@@ -0,0 +1,9 @@
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.#
+import math
+
+class Solution(object):
+    def mySqrt(self, x):
+    
+        sqrt_n = math.sqrt(x) 
+        rounded = int(math.floor(sqrt_n))
+        return rounded
